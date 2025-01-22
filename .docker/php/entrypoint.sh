51ca532a97088c1e7d311e9470bdeb713f8c88b7
Ism1tha <xgalan@hotmail.com>
2025-01-23 00:33:35 +0100

    Add entrypoint script to install dependencies and start Vite dev server

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -7,7 +7,10 @@ chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # permissions for PHPMyAdmin
 mkdir -p /sessions
-
 chmod 777 /sessions
+
+# Install dependencies and run vite dev server
+echo "Installing dependencies and running vite dev server..."
+cd /var/www && npm install && npm run dev
 
 exec "$@"
