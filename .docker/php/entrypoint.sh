0d4c11407fbb1e7a3960c9ef5cee0e5e93337a71
Ismael Semmar Galvez <30930942+Ism1tha@users.noreply.github.com>
2025-01-23 11:47:59 +0100

    Disable filemode tracking in the repository on entrypoint

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 set -e
 
-# Set permissions for Laravel directories
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-
-# permissions for PHPMyAdmin
+# Permissions for PHPMyAdmin
 mkdir -p /sessions
 chmod 777 /sessions
 
+# Disable filemode for git (permission tracking)
+cd /var/www
+git config core.filemode false
+
 # Install dependencies and run vite dev server
-echo "Installing dependencies and running vite dev server..."
-cd /var/www && npm install && npm run dev &
+npm install && npm run dev &
+
+# Set permissions for Laravel directories
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 exec "$@"
